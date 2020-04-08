deaf9120ddca7b90045a92f39c38fbe36dcf365e
@albul-k <konstantin.albul@gmail.com>
2020-04-09 00:18:39 +0300

    item2

--- les1.py
+++ les1.py
@@ -23,3 +23,20 @@ while i <= i_max:
         num_list.append(num_input)
         i += 1
 print(f"Введенные числа {num_list}")
+
+print(""" 
+\n2. Пользователь вводит время в секундах. 
+Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс. 
+Используйте форматирование строк.
+\n""")
+
+while True:
+    seconds_input = input("Введите время в секнудах\n")
+    if seconds_input.isdecimal():
+        hours = int(seconds_input) // 3600
+        minutes = (int(seconds_input) % 3600) / 60
+        seconds = (int(seconds_input) % 3600) % 60
+        print(f"{int(hours):02d}:{int(minutes):02d}:{int(seconds):02d}")
+        break
+    else:
+        print("Введено некорректное значение. Повторите ввод")
