96fbc5d713da0f08ec1abd30f825e6ee682b7e11
@albul-k <konstantin.albul@gmail.com>
2020-04-09 00:40:25 +0300

    item3

--- les1.py
+++ les1.py
@@ -40,3 +40,17 @@ while True:
         break
     else:
         print("Введено некорректное значение. Повторите ввод")
+
+print(""" 
+\n3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
+Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
+\n""")
+
+while True:
+    num_input = input("Введите число\n")
+    if num_input.isdecimal():
+        result = int(num_input) + int(str(num_input)*2) + int(str(num_input)*3)
+        print(result)
+        break
+    else:
+        print("Введено некорректное значение. Повторите ввод")
