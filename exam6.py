434b4c667efe0874361ef6d378f7d5068244ea0f
@albul-k <konstantin.albul@gmail.com>
2020-04-13 00:45:42 +0300

    exam6: input of product properties

--- exam6.py
+++ exam6.py
@@ -4,3 +4,58 @@ print("""
 В кортеже должно быть два элемента — номер товара и словарь с параметрами (характеристиками товара: название, цена, количество, единица измерения). 
 Структуру нужно сформировать программно, т.е. запрашивать все данные у пользователя.
 """)
+
+properties = {
+    0: 'название',
+    1: 'цена',
+    2: 'количество',
+    3: 'ед'
+}
+
+l_prop = []
+i = 1
+while True:
+    print(f'Введите характеристики товара №{i}')
+    l = []
+    for key, value in properties.items():
+        tmp = input(f'{value}\n')
+        if not tmp == "":
+            l.append(tmp)
+        else:
+            print('Введено пустое значение. Повторите ввод\n')
+            continue
+    l_prop.append(l)
+    i += 1
+    a = input('Ввести характеристики еще одного товара y/n\n')
+    if a == "y":
+        continue
+    else:
+        break
+
+products = [
+    (ind_prod, {properties[key_prop]:prop for key_prop, prop in enumerate(prod)}) for ind_prod, prod in enumerate(l_prop)
+]
+print(products)
+
+# """
+# {
+#     “название”: [“компьютер”, “принтер”, “сканер”],
+#     “цена”: [20000, 6000, 2000],
+#     “количество”: [5, 2, 7],
+#     “ед”: [“шт.”]
+# }
+# """
+
+# products = [(0, {'название': 'aaa', 'цена': '10', 'количество': '5', 'ед': 'шт'}), (1, {
+#     'название': 'bbb', 'цена': '50', 'количество': '12', 'ед': 'шт'})]
+
+# products_an={}
+# for prop_key, prop_value in properties.items():
+#     l_prop = []
+#     for item in products:
+#         l_prop.append(item[1].get(prop_value))
+#     products_an.update(l_prop)
+
+# products_an = [
+#      {prop_value:item[1].get(prop_value) for item in products} for prop_key, prop_value in properties.items()
+#  ]
