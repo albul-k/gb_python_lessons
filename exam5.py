ba65dc7238814c88ba104c5ed57254788a990f0b
@albul-k <konstantin.albul@gmail.com>
2020-04-28 00:57:55 +0300

    exam5

--- exam5.py
+++ exam5.py
@@ -4,3 +4,50 @@
 В каждом из классов реализовать переопределение метода draw. Для каждого из классов методы должен выводить уникальное сообщение. 
 Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
 """
+
+
+class Stationery:
+    title = ''
+
+    def __init__(self, title: str):
+        self.title = title
+
+    def draw(self):
+        return 'Запуск отрисовки.'
+
+
+class Pen(Stationery):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'Ручка. Имя={self.title}'
+
+
+class Pencil(Stationery):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'Карандаш. Имя={self.title}'
+
+
+class Handle(Stationery):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'Маркер. Имя={self.title}'
+
+
+stationery = Stationery('def')
+print(stationery.draw())
+
+pen = Pen('Красная ручка')
+print(pen.draw())
+
+pencil = Pencil('Синий карандаш')
+print(pencil.draw())
+
+handle = Handle('Желтый маркер')
+print(handle.draw())
